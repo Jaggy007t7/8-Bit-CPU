55c0b840b5629694ec628c81e923612020e8b3e0
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-03-14 00:56:41 +0530

    Implement 4-bit counter with reset functionality

--- Datapath Design/Programme Counter/PC.v
+++ Datapath Design/Programme Counter/PC.v
@@ -1,1 +1,25 @@
+// 4 Bit counter;
+module counter(
+    input clk,reset, //asynchronous reset.
+    output reg [3:0]count
+);
 
+initial begin 
+    count=4'b1111;
+end
+
+always @(posedge clk or posedge reset)begin 
+    if(reset)begin 
+        count=4'b0000;
+    end
+    else begin 
+    if(count==4'b1111)begin 
+        count<=4'b0000;
+    end
+    else begin 
+        count = count+1'b1;
+    end
+    end
+end
+
+endmodule
