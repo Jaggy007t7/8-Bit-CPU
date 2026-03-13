881a2978ee736e341cc50defe76bf53dcef2c32b
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-03-14 00:56:08 +0530

    Implement octal bus transceiver simulation module
    Added simulation code for the Octal Bus Transceiver module.

--- Datapath Design/Octal Bus tranciever/tristate_buffer.v
+++ Datapath Design/Octal Bus tranciever/tristate_buffer.v
@@ -1,1 +1,12 @@
+// This is the simulation code for IC - Octal Bus Transceiver;
+module oct_bus_ts(
+    input DIR,
+    input [7:0]a,
+    input [7:0]b,
+    output [7:0]a_out,
+    output [7:0]b_out
+);
 
+assign a_out = (DIR)?a:b;
+assign b_out = (DIR)?a:b;
+endmodule
